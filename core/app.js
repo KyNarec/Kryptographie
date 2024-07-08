39d0fa37516d2ab03d5d3448b4b67512d1494fde
S211054 <jim48@gmx.de>
2024-07-08 12:08:40 +0200

    Folder Geordnet und custom-comps erstellt

---
+++ core/app.js
@@ -0,0 +1,5 @@
+import Component from "/modules/components.js";
+import waitForElm from "/utils/waitForElement.js";
+
+// define custom elements
+customElements.define('custom-comp', Component);
---
+++ modules/components.js
@@ -0,0 +1,62 @@
+import waitForElm from "/utils/waitForElement.js";
+
+export default class Component extends HTMLElement {
+
+    /**@type {ShadowRoot} */
+    shadow;
+
+    static get observedAttributes() {
+        return ['name'];
+    }
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+
+    }
+
+    disconnectedCallback() {}
+
+    adoptedCallback() {}
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case 'name':
+                if (oldValue == newValue) {
+                    break;
+                }
+                console.debug(name+'changed from '+oldValue+' to '+newValue);
+                this.loadComponent(newValue);
+                break;
+            default:
+                console.error(`No behavior for attribute-change of ${name} defined`);
+        }
+    }
+
+    async loadComponent(name) {
+        if(name == 0) {
+            return;
+        }
+        const response = await fetch('/components/'+name+'.html');
+        if (!response.ok) throw Error('Could not fetch component: '+name);
+
+        const tempDiv  = document.createElement('div');
+        tempDiv.innerHTML = await response.text(); 
+
+        const fragment = document.createDocumentFragment();
+        const childCount = tempDiv.childElementCount;
+
+        for (let i = 0; i < childCount; i++) {
+            fragment.appendChild(tempDiv.children[0]);
+        }
+        this.innerHTML = '';
+        this.appendChild(fragment);
+        //this.shadow.appendChild(fragment);
+
+        // const script = await fetch('/components/'+name+'.js');
+        // if (!script.ok) return;
+        // await eval(await script.text());
+    }
+}
